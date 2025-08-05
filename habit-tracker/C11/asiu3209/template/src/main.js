a1cc6b74b876a4985c109a96689f782b0841e558
Aaron Siu <aarons3209@gmail.com>
2025-08-04 21:01:49 -0400

    Implemented Mark Complete and Incomplete buttons, added a streak

--- habit-tracker/C11/asiu3209/template/src/main.js
+++ habit-tracker/C11/asiu3209/template/src/main.js
@@ -4,23 +4,23 @@ const allHabits = [
   {
     id: 0,
     name: "Exercise",
-    frequency: "45",
+    frequency: 45,
     dateAdded: "2023-10-01",
-    progress: 65,
+    progress: 34,
   },
   {
     id: 1,
     name: "Read",
-    frequency: "20",
+    frequency: 20,
     dateAdded: "2023-10-02",
-    progress: 30,
+    progress: 11,
   },
   {
     id: 2,
     name: "Meditate",
-    frequency: "365",
+    frequency: 30,
     dateAdded: "2023-10-03",
-    progress: 80,
+    progress: 21,
   },
 ];
 
@@ -35,8 +35,13 @@ function displayHabits() {
       <h3>${habit.name}</h3>
       <div>Frequency: ${habit.frequency}</div>
       <div class="habit-progress">
-          <progress value="${habit.progress}" max="100">${habit.progress}%</progress>
-        <div class="progress-text">${habit.progress}% Complete</div>
+          <progress value="${
+            (habit.progress / habit.frequency) * 100
+          }" max="100">${habit.progress}%</progress>
+        <div class="progress-text">Current Streak: ${habit.progress} (${(
+      (habit.progress / habit.frequency) *
+      100
+    ).toFixed(2)}%)</div>
       </div>
         <button class="mark-complete">Mark Complete</button>
         <button class="mark-incomplete">Mark Incomplete</button>
@@ -45,28 +50,47 @@ function displayHabits() {
     habitList.appendChild(newHabbit);
   }
 }
-
+//Handles creating new habits and adding them to the list
 document.getElementById("habit-form").addEventListener("submit", function (e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const habit = {
     id: habitID++,
     name: formData.get("habit-name"),
-    frequency: formData.get("habit-frequency"),
+    frequency: Number(formData.get("habit-frequency")),
     dateAdded: new Date().toISOString().split("T")[0], // Current date in YYYY-MM-DD format
     progress: 0,
   };
   allHabits.push(habit);
   console.log("New habit created:", habit);
   displayHabits();
-  e.reset();
+  e.target.reset();
 });
-
+//Deletes Habit Cards
 document.getElementById("habits-list").addEventListener("click", function (e) {
   if (e.target.classList.contains("delete-habit")) {
     const habitCard = e.target.closest(".habit-card");
     const id = parseInt(habitCard.id);
     deleteCard(id);
+  }
+  if (e.target.classList.contains("mark-complete")) {
+    const habitCard = e.target.closest(".habit-card");
+    for (let i = 0; i < allHabits.length; i++) {
+      if (allHabits[i].id === parseInt(habitCard.id)) {
+        allHabits[i].progress++;
+        displayHabits();
+        return;
+      }
+    }
+  } else if (e.target.classList.contains("mark-incomplete")) {
+    const habitCard = e.target.closest(".habit-card");
+    for (let i = 0; i < allHabits.length; i++) {
+      if (allHabits[i].id === parseInt(habitCard.id)) {
+        allHabits[i].progress = 0;
+        displayHabits();
+        return;
+      }
+    }
   }
 });
 
@@ -81,4 +105,4 @@ function deleteCard(id) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", displayHabits);
+document.addEventListener("DOMContentLoaded", displayHabits());
